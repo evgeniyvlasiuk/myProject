05b86753dd9bdf1d367aa4e7064daa22c0aa9c43
evgeniyvlasiuk <vlasiuk.zp@gmail.com>
2016-06-14 15:02:09 +0300

    myProject

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,44 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'My project'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('My project')
-    end
-
-   	it "should have the right title" do
-	 	 visit '/static_pages/home'
-	 	 expect(page).to have_title("My project | Home")
-	end
-
+    it { should have_content('My project') }
+    it { should have_title(full_title('')) }
+    it { should have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-	 	 visit '/static_pages/help'
-	 	 expect(page).to have_title("My project | Help")
-	end
-
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
+
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
 
-    it "should have the right title" do
-	 	 visit '/static_pages/about'
-	 	 expect(page).to have_title("My project | About")
-	end
+  describe "Contact page" do
+    before { visit contact_path }
 
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
