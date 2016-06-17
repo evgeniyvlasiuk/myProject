c19e3f96540f9e6391c3c06fe525480852c123f4
evgeniyvlasiuk <vlasiuk.zp@gmail.com>
2016-06-17 12:14:46 +0300

    myProject

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,10 @@ class User < ActiveRecord::Base
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
-
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
